83999c32b008fd4d65a891745e7ab254ac1b92f3
franciscoazevedo <franciscoovazevedo@gmail.com>
2017-10-30 22:06:19 +0000

    "the same-same as the last commit"

--- https:/github.com/franciscoovazevedo/searchNews.git/PCD - GoogleNews/src/News.java
+++ https:/github.com/franciscoovazevedo/searchNews.git/PCD - GoogleNews/src/News.java
@@ -3,6 +3,10 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
+import java.util.StringTokenizer;
+import java.util.regex.Pattern;
+
+import javax.xml.soap.Text;
 
 public class News {
 	private int id;
@@ -17,6 +21,23 @@ public class News {
 			title = scanner.nextLine();
 		while(scanner.hasNextLine())
 			content = scanner.nextLine();
+	}
+	
+	public boolean hasWord(String word) {
+		if(title.toLowerCase().contains(word.toLowerCase()) || content.toLowerCase().contains(word.toLowerCase()))
+			return true;
+		
+		return false;
+	}
+	
+	public int howManyTimesWord(String word) {
+		if(hasWord(word)){
+			int counter = content.toLowerCase().split("\\b" + word.toLowerCase() + "\\b").length - 1;
+			counter += title.toLowerCase().split("\\b" + word.toLowerCase() + "\\b").length - 1;
+			return counter;
+		}
+		
+		return 0;
 	}
 	
 	public String getTitle() {
